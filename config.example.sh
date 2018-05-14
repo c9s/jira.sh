40829a547f1f87c14cc64f78be2dbe0bc5e29523
c9s <yoanlin93@gmail.com>
2018-05-14 13:00:45 +0800

    add the example config

---
+++ config.example.sh
@@ -0,0 +1,7 @@
+# go to this page to get the token
+# https://id.atlassian.com/manage/api-tokens
+JIRA_USER="user@test.com"
+JIRA_PASS="secret"
+
+JIRA_DOMAIN="yoursite.atlassian.net"
+JIRA_PROJECT_CODE="AA"
